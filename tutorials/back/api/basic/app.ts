a6bcf2d977631a3c748016ecb06aa2fd76fd957a
walid.gbil <walid.gbil@student.vinci.be>
2024-09-30 22:36:09 +0200

    new: ex1.2

---
+++ tutorials/back/api/basic/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+import usersRouter from "./routes/users";
+import pizzaRouter from "./routes/pizzas";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/users", usersRouter);
+app.use("/pizzas", pizzaRouter);
+  
+
+export default app;
